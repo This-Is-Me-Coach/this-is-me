4b320cceb669c0599f834b26b7218e0b5fdab59f
Darrel Pol <poldarre.dev@gmail.com>
2022-05-07 18:53:16 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Amplify, Auth} from 'aws-amplify'
+import config from './src/aws-exports'
+Amplify.configure(config);
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text
+} from 'react-native';
+import { withAuthenticator } from 'aws-amplify-react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  async componentDidMount() {
+    const user = await Auth.currentAuthenticatedUser();
+    console.log('user:', user);
+  }
+  signOut = () => {
+    Auth.signOut()
+      .then(() => this.props.onStateChange('signedOut'))
+      .catch(err => console.log('err: ', err))
+  }
+  render() {
+    return(
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Hello, This is Me!</Text>
+        <Text onPress={this.signOut}>Sign Out</Text>
+      </SafeAreaView>
+    ) 
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  title: {
+    fontSize: 28
+  }
+})
+
+export default withAuthenticator(App, {
+  includeGreetings: true
+})
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "thisismee0402fc0": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
