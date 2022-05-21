530c81e2c6c046ac9fa0367d206e55f9c44eef80
Darrel Pol <poldarre.dev@gmail.com>
2022-05-21 15:31:59 -0500

    TIM-190: adding customized login

--- App.js
+++ App.js
@@ -1,74 +1,25 @@
-import {Amplify, Auth, API, graphqlOperation} from 'aws-amplify'
-import config from './src/aws-exports'
+import { Amplify } from 'aws-amplify'
+
+import 'react-native-gesture-handler';
+import { AppRegistry } from 'react-native';
+import { expo as expoConfig } from './app.json';
+import { registerRootComponent } from 'expo';
+
+import config from './src/aws-exports';
+
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import App from './src/Main';
+
 Amplify.configure(config);
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import { withAuthenticator } from 'aws-amplify-react-native'
-import { listTodos } from './src/graphql/queries';
 
-class App extends React.Component {
-  
-  state = {
-    todos: []
-  }
-
-  async getTodos() {
-    try {
-      const todoList = await API.graphql(graphqlOperation(listTodos));
-      console.log('todo list:', todoList);
-      this.setState({
-        todos: todoList.data.listTodos.items
-      });
-    } catch (error) {
-      console.log('unable to fetch todo list...', error);
-    }
-  }
-
-  async componentDidMount() {
-    await this.getTodos();
-  }
-  signOut = () => {
-    Auth.signOut()
-      .then(() => this.props.onStateChange('signedOut'))
-      .catch(err => console.log('err: ', err))
-  }
-  render() {
-    return(
-      <SafeAreaView style={styles.container}>
-        {
-          this.state.todos.map((todo, index) => (
-            <View key={index} style={styles.item}>
-              <Text style={styles.name}>{todo.name}</Text>
-              <Text style={styles.description}>{todo.description}</Text>
-            </View>
-          ))
-        }
-        <Text style={styles.title}>Hello, This is Me!</Text>
-        <Text onPress={this.signOut}>Sign Out</Text>
-      </SafeAreaView>
-    ) 
-  }
+function AppWithNavigationContainer() {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  item: { padding: 10 },
-  name: { fontSize: 20 },
-  description: { fontWeight: '600', marginTop: 4, color: 'rgba(0, 0, 0, .5)' },
-  title: {
-    fontSize: 28
-  }
-})
-
-export default withAuthenticator(App, {
-  includeGreetings: true
-})
+registerRootComponent(AppWithNavigationContainer);
