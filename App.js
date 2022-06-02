83d35ad3266f6d127407cce1357c317c7d1ed0dc
xanderjakeq <xanderjakeq@gmail.com>
2022-06-02 19:31:18 -0400

    TIM-190: Show AppIntro the first time app is opened

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import { Amplify } from 'aws-amplify'
+import { Amplify } from 'aws-amplify';
 
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import { expo as expoConfig } from './app.json';
 import { registerRootComponent } from 'expo';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import config from './src/aws-exports';
 
@@ -11,15 +12,50 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import App from './src/Main';
+import AppIntro from './src/pages/AppIntro';
 
 Amplify.configure(config);
 
 function AppWithNavigationContainer() {
-  return (
+  const [initialized, setInitialized] = React.useState(false);
+
+  React.useEffect(() => {
+    isInitialized().then((val) => setInitialized(val === 'true'));
+  }, []);
+
+  return initialized ? (
     <NavigationContainer>
-        <App />
+      <App />
     </NavigationContainer>
+  ) : (
+    <AppIntro
+      next={() => {
+        initialize();
+        setInitialized(true);
+      }}
+    />
   );
 }
+
+const initialize = async () => {
+  try {
+    await AsyncStorage.setItem('initialized', 'true');
+  } catch (e) {
+    // error reading value
+    console.log(e);
+  }
+};
+
+const isInitialized = async () => {
+  try {
+    const value = await AsyncStorage.getItem('initialized');
+    if (value !== null) {
+      return value;
+    }
+  } catch (e) {
+    // error reading value
+    console.log(e);
+  }
+};
 
 registerRootComponent(AppWithNavigationContainer);
